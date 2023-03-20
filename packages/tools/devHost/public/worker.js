2bcd2ca89ad345a027d724ee0a7e32e6ffce3a31
Carolina Herbster <carolhmj@gmail.com>
2023-03-20 17:54:21 -0300

    Experiment 2 with more complex scenario

--- packages/tools/devHost/public/worker.js
+++ packages/tools/devHost/public/worker.js
@@ -1,10 +1,32 @@
+let context;
+let bitmap;
+let width;
+let height;
 onmessage = (e) => {
-    console.log("Message received from main script");
+    // console.log("Message received from main script");
     // const workerResult = `Result: ${e.data[0] * e.data[1]}`;
     // console.log("Posting message back to main script");
     // postMessage(workerResult);
-    const context = e.data.canvas.getContext('2d');
-    const bitmap = e.data.bitmap;
-    context.drawImage(bitmap, 0, 0, 512, 512);
+    const type = e.data.type;
+    if (type === "start") {
+        console.log('received start message', e.data.canvas, e.data.bitmap);
+        context = e.data.canvas.getContext('2d');
+        bitmap = e.data.bitmap;
+        width = e.data.width;
+        height = e.data.height;
+    } else if (type === "draw") {
+        if (!context || !bitmap) {
+            console.error('context or bitmap is not initialized');
+            return;
+        }
+        context.clearRect(0, 0, width, height);
+        const positionsList = e.data.positionsList;
+        // console.log('received draw message', positionsList);
+        for (let i = 0; i < positionsList.length; i++) {
+            const [x, y] = positionsList[i];
+            // console.log('positions are', x, y);
+            context.drawImage(bitmap, x, y, 25, 25);
+        }
+    }
     postMessage("done");
 };
--- packages/tools/devHost/src/createScene.ts
+++ packages/tools/devHost/src/createScene.ts
@@ -1,16 +1,20 @@
+/* eslint-disable no-var */
 /* eslint-disable-next-line import/no-internal-modules */
 import { canvas, engine } from "./index";
 import "@dev/loaders";
 import "@tools/node-editor";
-import { Inspector } from "@dev/inspector";
-import { DynamicTexture, FreeCamera, HemisphericLight, MeshBuilder, StandardMaterial, Vector3, Scene } from "@dev/core";
+import type { Mesh} from "@dev/core";
+import { SceneInstrumentation , Layer, DynamicTexture, HemisphericLight, MeshBuilder, StandardMaterial, Vector3, Scene, ArcRotateCamera, Color3, TransformNode } from "@dev/core";
+
+import { AdvancedDynamicTexture, Container, Control, StackPanel, TextBlock, Image } from "@dev/gui";
 
 export const createScene = async function () {
     // This creates a basic Babylon Scene object (non-mesh)
     const scene = new Scene(engine);
 
+    const radius = 20;
     // This creates and positions a free camera (non-mesh)
-    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+    const camera = new ArcRotateCamera("cam", -Math.PI / 3, Math.PI / 3, radius * 2, new Vector3(0, 0, 0), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(Vector3.Zero());
@@ -24,51 +28,157 @@ export const createScene = async function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape. Params: name, options, scene
-    const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
     // Our built-in 'ground' shape. Params: name, options, scene
-    const ground = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    MeshBuilder.CreateGround("ground", { width: radius, height: radius }, scene);
 
-    const textureGround = new DynamicTexture("dynamic texture", 512, scene, true);
-    const textureOffscreen = (textureGround._canvas as HTMLCanvasElement).transferControlToOffscreen();
-    // const textureContext = textureGround.getContext();
-    // const img = new Image();
-    // img.src = 'amiga.jpg';
-    // img.onload = function() {
-    //     //Add image to dynamic texture
-    //     textureContext.drawImage(this, 0, 0);
-    //     textureGround.update();
+    // const plane = MeshBuilder.CreatePlane("plane", { width: radius, height: radius }, scene);
+    const width = canvas!.clientWidth;
+    const height = canvas!.clientHeight;
 
-    //     textureContext.drawImage(this, 10, 490, 10, 12, 156, 136, 200, 220)
-    //     textureGround.update();	
-    // }
-    const textureWorker = new Worker("worker.js");
-    const img = new Image();
-    img.src = 'test-sprite-2k.png';
-    img.onload = async function() {
-        //Add image to dynamic texture
-        // context.drawImage(this, 0, 0);
-        const bitmap = await createImageBitmap(img);
-        textureWorker.postMessage({ canvas: textureOffscreen, bitmap }, [textureOffscreen, bitmap]);
-    };
+    const objs: Mesh[] = [];
+    for (let i = 0; i < 10; i++) {
+        const position = new Vector3(i - radius / 2, 0, Math.random() * radius - radius / 2);
 
-    textureWorker.onmessage = (e) => {
-        console.log("Message received from worker", e);
-        setTimeout(() => {
-            textureGround.update();
-        }, 2000);
+        const transformNode = new TransformNode("sphere" + i, scene);
+        transformNode.position = position;
+
+        const obj = MeshBuilder.CreateSphere("obj" + i, { diameter: 2 });
+        obj.parent = transformNode;
+        obj.position.y = 1;
+
+        const material = new StandardMaterial("mat");
+        material.diffuseColor = Color3.Random();
+        obj.material = material;
+
+        objs.push(obj);
     }
 
-    const materialGround = new StandardMaterial("Mat", scene);
-    materialGround.diffuseTexture = textureGround;
-    ground.material = materialGround;
+    const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    const useWorker = true;
 
-    Inspector.Show(scene, {});
-    return scene;
+    if (useWorker) {
+        const textureGround = new DynamicTexture("dynamic texture", { width, height }, scene, true);
+        const textureOffscreen = (textureGround._canvas as HTMLCanvasElement).transferControlToOffscreen();
 
+        const layer = new Layer("layer", null, scene, false);
+        layer.texture = textureGround;
+        const textureWorker = new Worker("worker.js");
+        const img = document.createElement("img");
+        img.src = "test-sprite-2k.png";
+        img.onload = async function () {
+            //Add image to dynamic texture
+            // context.drawImage(this, 0, 0);
+            const bitmap = await createImageBitmap(img);
+            textureWorker.postMessage({ type: "start", canvas: textureOffscreen, bitmap, width, height }, [textureOffscreen, bitmap]);
+
+            scene.onBeforeRenderObservable.add(() => {
+                // For each object, get its position on the camera plane
+                const positionsList: [number, number][] = [];
+                for (let i = 0; i < objs.length; i++) {
+                    const obj = objs[i];
+                    const transformNode = obj.parent as TransformNode;
+                    const position = transformNode.position;
+                    const cameraPlanePosition = Vector3.Project(
+                        position,
+                        camera.getViewMatrix(),
+                        camera.getProjectionMatrix(),
+                        camera.viewport.toGlobal(engine.getRenderWidth(), engine.getRenderHeight())
+                    );
+                    const x = cameraPlanePosition.x;
+                    const y = cameraPlanePosition.y;
+                    // console.log('position', x, y);
+                    positionsList.push([x, y]);
+                }
+                textureWorker.postMessage({ type: "draw", positionsList });
+            });
+        };
+
+        textureWorker.onmessage = (e) => {
+            textureGround.update();
+        };
+    } else {
+        for (let i = 0; i < objs.length; i++) {
+            buildLabel(advancedTexture, objs[i].parent as TransformNode);
+        }
+    }
+
+    const instrumentation = new SceneInstrumentation(scene);
+    instrumentation.captureFrameTime = true;
+    instrumentation.captureRenderTime = true;
+
+    const fpsCounter = new TextBlock("fps", "");
+    fpsCounter.fontSize = "24px";
+    fpsCounter.color = "black";
+    fpsCounter.outlineColor = "white";
+    fpsCounter.outlineWidth = 2;
+    fpsCounter.top = "300px";
+    advancedTexture.addControl(fpsCounter);
+    scene.onAfterRenderObservable.add(() => {
+        fpsCounter.text = `absolute fps: ${(1000 / instrumentation.frameTimeCounter.lastSecAverage).toFixed(2)}\n`;
+    });
+
+    // Inspector.Show(scene, {});
     return scene;
 };
+
+var buildLabel = (advancedTexture: AdvancedDynamicTexture, transformNode: TransformNode) => {
+    const stemHeight = 100;
+
+    // Root container is just 1x1 placement for the top-left corner of the flag UI.
+    var container = new Container();
+    container.widthInPixels = 1;
+    container.heightInPixels = 1;
+    container.background = "#00000000";
+    container.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+    container.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+    container.clipChildren = false;
+    container.clipContent = false;
+    container.setPadding(0, 0, 0, 0);
+    advancedTexture.addControl(container);
+    container.linkWithMesh(transformNode);
+
+    // This contentPanel contains all the content (icon/label) of the flag.
+    const contentPanel = new StackPanel();
+    contentPanel.adaptWidthToChildren = true;
+    contentPanel.adaptHeightToChildren = true;
+    contentPanel.isPointerBlocker = false;
+    contentPanel.isVertical = false;
+    contentPanel.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+    contentPanel.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+    contentPanel.clipChildren = false;
+    contentPanel.clipContent = false;
+    contentPanel.background = "#00000000";
+    //contentPanel.background = "#00FF0066";
+    contentPanel.setPadding(0, 0, 0, 0);
+    container.addControl(contentPanel);
+
+    // const icon = new Image("icon", "textures/grass.png");
+    const icon = new Image("icon", "https://raw.githubusercontent.com/carolhmj/quick-demos/main/assets/textures/sprites/test-sprite-2k.png");
+    icon.widthInPixels = 50;
+    icon.heightInPixels = icon.widthInPixels;
+    icon.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+    icon.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+    icon.isPointerBlocker = true;
+    icon.isHitTestVisible = true;
+    icon.shadowBlur = 4.0;
+    icon.shadowOffsetY = 2.0;
+    const padding = 2.0;
+    icon.setPaddingInPixels(padding, padding, padding, padding);
+    contentPanel.addControl(icon);
+
+    // var primaryLabel = new TextBlock();
+    // primaryLabel.isVisible = true;
+    // primaryLabel.isHitTestVisible = false;
+    // primaryLabel.color = "#FFFFFFFF";
+    // primaryLabel.text = text;
+    // primaryLabel.fontSizeInPixels = 40;
+    // primaryLabel.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+    // primaryLabel.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+    // primaryLabel.resizeToFit = true;
+    // primaryLabel.isPointerBlocker = true;
+    // primaryLabel.fontFamily = "Segoe UI";
+    // contentPanel.addControl(primaryLabel); 
+
+    container.linkOffsetYInPixels = -stemHeight;
+    container.linkOffsetXInPixels = -0.5 * icon.widthInPixels;
+}
